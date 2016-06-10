9fae182cbe1e0b42727df24c7a9f270a7a62dbc5
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2016-06-10 11:37:18 -0500

    cambio de estilo form fact sheets y primer fact

--- wp-content/themes/lucian/page-fact-sheet-download.php
+++ wp-content/themes/lucian/page-fact-sheet-download.php
@@ -25,24 +25,24 @@
 			<div class="[ col-xs-12 col-sm-3 ]"></div>
 			<div class="[ col-xs-12 col-sm-6 ][ margin-bottom ]">
 				<div class="testimonial-form-container">
-					<p class="[ margin-bottom--large ]"><strong>File to download:</strong><span> <?php echo $pdf['title'] ?></span></p>
+					<p class="[ margin-bottom--large ]"><strong class="[ color-secondary ]">File to download:</strong><span> <?php echo $pdf['title'] ?></span></p>
 					<div>
 						<form class="[ js-fact-sheets-form ][ margin-bottom--large ]" name="new_post" method="post" enctype="multipart/form-data" novalidate="novalidate">
 							<p>
-								<label>Name</label>
-								<input type="text" class="full required requiredField" value="" id="kt-feedback-post-title" name="name">
+								<label><strong>Name</strong></label>
+								<input type="text" class="full required requiredField [ background-gray ]" value="" id="kt-feedback-post-title" name="name">
 							</p>
 							<p>
-								<label>Email</label>
-								<input type="email" class="full required requiredField" value="" id="kt-feedback-post-email" name="email">
+								<label><strong>Email</strong></label>
+								<input type="email" class="full required requiredField [ background-gray ]" value="" id="kt-feedback-post-email" name="email">
 							</p>
 							<p>
-								<label>Position</label>
-								<input type="text" class="full required requiredField" id="kt-feedback-post-position" value="" name="position">
+								<label><strong>Position</strong></label>
+								<input type="text" class="full required requiredField [ background-gray ]" id="kt-feedback-post-position" value="" name="position">
 							</p>
 							<p>
-								<label>Company</label>
-								<input type="text" class="full required requiredField" value="" id="kt-feedback-post-company" name="company">
+								<label><strong>Company</strong></label>
+								<input type="text" class="full required requiredField [ background-gray ]" value="" id="kt-feedback-post-company" name="company">
 							</p>
 							<input type="hidden" name="pdf_url" value="<?php echo $pdf['url'] ?>">
 							<input type="hidden" name="pdf_title" value="<?php echo $pdf['title'] ?>">
